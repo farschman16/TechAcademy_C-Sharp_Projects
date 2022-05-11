f02cc25b3b2ee3f67743f1c682cfb464a5aa10a1
Farschman16 <farschman16@gmail.com>
2022-05-10 21:24:58 -0400

    Acme Accounting Commit

---
+++ AcmeAccounting/AcmeAccounting/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AcmeAccounting
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems \n Remember, We're \"accounting\" on you!");
+            Console.Read();
+        }
+    }
+}
