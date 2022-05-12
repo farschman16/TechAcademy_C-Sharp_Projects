cb4de8c87f0c4eaf3e41785a7bc0a70be25307a1
Farschman16 <farschman16@gmail.com>
2022-05-12 14:18:53 -0400

    Tech Adacemy Daily Report Submission Assignment

---
+++ DailyReport/DailyReport/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DailyReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy \n Student Daily Report"); //writes the opening greeting
+
+            Console.WriteLine("What is your name?"); //string, asks person to write a string that is their name
+            string yourName = Console.ReadLine();
+
+            Console.WriteLine("What course are you on?"); //another string asking to write a string of the course
+            string currentCourse = Console.ReadLine();
+
+            Console.WriteLine("What page number?"); //string asking for page number
+            string pageNumber = Console.ReadLine();
+            int pageNum = Convert.ToInt32(pageNumber); //converts from string to integer
+
+            Console.WriteLine("Do you need help with anything? \n Please answer \"True\" or \"False\".");
+            string needHelp = Console.ReadLine(); //could also be a bool function with different string WriteLine for each
+            Console.WriteLine("You've answered " + needHelp + " to needing additional help at this time.");
+
+            Console.WriteLine("Were there any positive experiences you'd like to share? \n Please give specifics.");
+            string posEx = Console.ReadLine();
+
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string feedBack = Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today?");
+            string hrsStudy = Console.ReadLine(); //answer as a string
+            int hrStudyInt = Convert.ToInt32(hrsStudy); //convert the string to an integer
+            Console.WriteLine("You've studied " + hrStudyInt + "hrs today!"); //use that integer in the displayed text
+
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this Shortly. \n Have a Great Day!");
+
+            //end of program
+        }
+    }
+}
---
+++ VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // hit ctrl + k + c to comment out lines you've highlighted. ctrl + k + u uncomments the highlighted lines.
+
+            //This is a program
+            Console.WriteLine("What is your name?");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("Your name is " + yourName);
+            Console.ReadLine();
+
+            Console.WriteLine("What is your favorite number?");
+            string favoriteNumber = Console.ReadLine();
+            int favNum = Convert.ToInt32(favoriteNumber); //converts from string to integer
+            int total = favNum + 5;
+            Console.WriteLine("Your favotie number plus 5 is: " + total);
+            Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemperature = -23;
+            char questionMark = '\u2103';
+
+            decimal moneyInBank = 100.5m; //use decimal for financials
+
+            double heightInCentimeters = 211.23451;
+
+            float secondsLeft = 1.62f;
+
+            short temperatureOnMars = -341;
+
+            string myName = "Matt";
+
+            int currentAge = 37;
+            string yearsOld = currentAge.ToString(); //this converts from integer to string
+
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining); //converts from boolean to string
+
+            Console.WriteLine(questionMark);
+            Console.ReadLine();
+        }
+    }
+}
