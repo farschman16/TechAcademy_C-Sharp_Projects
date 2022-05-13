6a2ffcd822d79be1a1bfe5bd1023cff795b6689f
Farschman16 <farschman16@gmail.com>
2022-05-13 17:30:06 -0400

    BooleanLogicSubmission

---
+++ BooleanLogic/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(true && false);
+            Console.WriteLine(true && true); //and function
+            Console.WriteLine(false && false);
+
+            Console.WriteLine(true || true);
+            Console.WriteLine(true || false); //or function
+            Console.WriteLine(false || false);
+
+            Console.WriteLine(true == true);
+            Console.WriteLine(true == false); //equal to
+            Console.WriteLine(false == false);
+
+            Console.WriteLine(true != true);
+            Console.WriteLine(true != false); // NOT true
+            Console.WriteLine(false != false);
+
+            Console.WriteLine(true ^ true);
+            Console.WriteLine(true ^ false); // called xor is true only if one of these things is true
+            Console.WriteLine(false ^ false);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BooleanLogicInsurance/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BooleanLogicInsurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Car Insurance Approval Program \n Please answer the following questions (honestly, of course)");
+            Console.WriteLine("What is your age?"); //asks for age
+            string personAge = Console.ReadLine(); //takes in user's response as a string
+            int personAgeInt = Convert.ToInt32(personAge); //converts response to an integer
+            Console.WriteLine("Your age is " + personAgeInt); //writes their response in console
+
+            Console.WriteLine("Have you ever had a DUI? (please answer \"yes\" or \"no\"");
+            string personDUI = Console.ReadLine();
+            Console.WriteLine("You answered " + personDUI);
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string personTickets = Console.ReadLine();
+            int personTicketsInt = Convert.ToInt32(personTickets); //converts response to an integer
+            Console.WriteLine("You have had " + personTicketsInt + " speeding tickets.");
+
+            bool isQualified = (personAgeInt > 15) && (personDUI == "no") && (personTicketsInt <= 3); //states that all 3 qualifications must return "true" for the boolean to be true.
+            Console.WriteLine("Are you qualified for this insurance?");
+            Console.WriteLine(isQualified);
+        }
+    }
+}
