69fbf08623a1493945225b266fc3d742503752c6
Farschman16 <farschman16@gmail.com>
2022-05-15 12:27:09 -0400

    Loops Submission

---
+++ Loops/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            bool isGuessed = number == 12; //states that the correct response is 12
+            do //do this while loop no matter what
+            {
+                switch (number) //switch loop
+                {
+                    case 62: //states "in the case the user's input is 62..."
+                        Console.WriteLine("You guessed 62. Try again."); //writes this line, and tells them to guess again
+                        Console.WriteLine("Guess a number"); //gues again
+                        number = Convert.ToInt32(Console.ReadLine()); //converts their input to integer again
+                        break; //breaks the loop, otherwise it would just keep coninuing to the next case. Instead, starts again
+                    case 29:
+                        Console.WriteLine("You guessed 29. Try again.");
+                        Console.WriteLine("Guess a number");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 55:
+                        Console.WriteLine("You guessed 55. Try again.");
+                        Console.WriteLine("Guess a number");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 12:
+                        Console.WriteLine("You guessed 12. That is correct!");
+                        isGuessed = true; //since the correct answer is 12, changes the value of isGuessed to "true"
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong.");
+                        Console.WriteLine("Guess a number");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!isGuessed); //"While the variable 'isGuessed' is false"
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WhileLoop/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number between 1 and 20.");
+            int numCorrect = Convert.ToInt32(Console.ReadLine()); //converts answer to intenger
+
+            bool isGuessed = numCorrect == 16; //states that the correct response is 16, otherwise will be false
+            do //do this while loop no matter what
+            {
+                switch (numCorrect) //switch loop
+                {
+                    case 0: //states "in the case the user's input is less than 1..."
+                        Console.WriteLine("You guessed too low. Number needs to be above 0. Try again."); //writes this line, and tells them to guess again
+                        Console.WriteLine("Guess a number"); //guess again
+                        numCorrect = Convert.ToInt32(Console.ReadLine()); //converts their input to integer again
+                        break; //breaks the loop, otherwise it would just keep coninuing to the next case. Instead, starts again
+                    case 20:
+                        Console.WriteLine("You guessed 20. That is too high. Try again.");
+                        Console.WriteLine("Guess a number");
+                        numCorrect = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 40:
+                        Console.WriteLine("You guessed 40, and I said guess between 1 and 20. Do better. Try again.");
+                        Console.WriteLine("Guess a number");
+                        numCorrect = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 16:
+                        Console.WriteLine("You guessed 16. That is correct!");
+                        isGuessed = true; //since the correct answer is 16, changes the value of isGuessed to "true" and the while loop will end in the next lines
+                        break; //now that the "while"(below) is true, breaks out of the loop and continues through the program.
+                    default: //default for any other incorrect case not explicitly written in the loop
+                        Console.WriteLine("You are wrong.");
+                        Console.WriteLine("Guess a number");
+                        numCorrect = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!isGuessed); //"While the variable 'isGuessed' is false..." -this is changed to true when the correct answer is chosen.
+
+            Console.ReadLine();
+        }
+    }
+}
