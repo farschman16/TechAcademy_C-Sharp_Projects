442320affefe196602fa1ef3c2069541cfd55a1f
Farschman16 <farschman16@gmail.com>
2022-06-22 20:30:41 -0400

    Create the "Struct" file

---
+++ Struct/Number.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    public struct Number //created the class of Number as a struct
+    {
+        public decimal Amount { get; set; } //created a property for struct Number of Amount (decimal format)
+    }
+}
---
+++ Struct/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num1 = new Number(); //create new instance of "Number" named num1
+            num1.Amount = 9995; //assign a value to new variable
+            Console.WriteLine(num1.Amount); //print to console the variable's value of the Amount property
+            Console.ReadLine();
+        }
+    }
+}
