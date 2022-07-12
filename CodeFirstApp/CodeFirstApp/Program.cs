bfa002736e4c2eaf5d49c581b639a8e098761d0d
Farschman16 <farschman16@gmail.com>
2022-07-12 19:51:45 -0400

    EF Code First Database App

---
+++ CodeFirstApp/CodeFirstApp/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CodeFirstApp;
+
+using (var ctx = new SchoolContext())
+{
+    var stud = new Student() { StudentName = "James Webb" };
+
+    ctx.Students.Add(stud);
+    ctx.SaveChanges();
+}
